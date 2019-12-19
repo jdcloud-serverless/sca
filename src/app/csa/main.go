531eef3c956b0e59e9536e24a389c96c31351908
charleskangzq <244527896@qq.com>
2019-12-19 11:55:05 +0800

    add csa app and dependencies

---
+++ src/app/csa/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"csa"
+)
+
+func main() {
+	csa.Start()
+}
---
+++ vendor/src/git.jd.com/jcloud-api-gateway/jcloud-sdk-go/core/ResponseProcessor.go
@@ -0,0 +1,45 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ResponseProcessor interface {
+	Process(response *http.Response) ([]byte, error)
+}
+
+func GetResponseProcessor(method string) ResponseProcessor {
+	if method == MethodHead {
+		return &WithoutBodyResponseProcessor{}
+	} else {
+		return &WithBodyResponseProcessor{}
+	}
+}
+
+type WithBodyResponseProcessor struct {
+}
+
+func (p WithBodyResponseProcessor) Process(response *http.Response) ([]byte, error) {
+	defer response.Body.Close()
+	if response == nil {
+		return nil, errors.New("response is nil")
+	}
+	return ioutil.ReadAll(response.Body)
+}
+
+type WithoutBodyResponseProcessor struct {
+}
+
+func (p WithoutBodyResponseProcessor) Process(response *http.Response) ([]byte, error) {
+	if response == nil {
+		return nil, errors.New("response is nil")
+	}
+	requestId := response.Header.Get(HeaderJdcloudRequestId)
+	if requestId != "" {
+		return []byte(fmt.Sprintf(`{"requestId":"%s"}`, requestId)), nil
+	}
+	return nil, errors.New("can not get requestId in HEAD response")
+}
