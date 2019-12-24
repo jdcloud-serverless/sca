9194d7fa1c8e949fcece9bc9d207d3cad1ead262
baoshenghua <baoshenghua@jd.com>
2019-12-24 14:31:51 +0800

    modify function privilege

--- commands/local/image/python36/start_server.sh
+++ commands/local/image/python36/start_server.sh
@@ -6,6 +6,8 @@ export LC_ALL=en_US.utf8
 # user
 groupadd -g 1000 function
 useradd -u 1000 -g 1000 function
+usermod -aG input function
+chown -R function:function /function
 
 #start wsgi
 /usr/local/bin/uwsgi --ini /function/wsgi_server.ini
