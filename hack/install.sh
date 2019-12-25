df6ad989e14679a82e8d04b9d1987e7c16323a5e
charleskangzq <244527896@qq.com>
2019-12-25 15:40:20 +0800

    update install.sh

--- hack/install.sh
+++ hack/install.sh
@@ -3,5 +3,5 @@ wget -P ~/.sca/ https://github.com/jdcloud-serverless/sca/releases/download/v0.0
 
 chmod +777 ~/.sca/sca
 
-echo "export PATH=$PATH:$HOME/.sca" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+echo "PATH=$PATH:~/.sca" >> ~/.bashrc
+echo "export PATH" >> ~/.bashrc
