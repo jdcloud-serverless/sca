837206f7b8356585b303ab27a8c7f982e80c1e75
charleskangzq <244527896@qq.com>
2019-12-27 17:50:30 +0800

    Update install-linux.sh

--- hack/install-linux.sh
+++ hack/install-linux.sh
@@ -1,5 +1,5 @@
 mkdir ~/.sca
-wget -P ~/.sca/sca https://github.com/jdcloud-serverless/sca/releases/download/v0.0.1/sca-linux
+wget -O ~/.sca/sca https://github.com/jdcloud-serverless/sca/releases/download/v0.0.1/sca-linux
 
 chmod +777 ~/.sca/sca
 
