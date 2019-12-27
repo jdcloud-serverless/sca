ffcd7fa03cb86a0b58b21cf05a42dc1c295c91f8
charleskangzq <244527896@qq.com>
2019-12-27 17:56:29 +0800

    Update install-mac.sh

--- hack/install-mac.sh
+++ hack/install-mac.sh
@@ -1,5 +1,5 @@
 mkdir ~/.sca
-wget -P ~/.sca/sca https://github.com/jdcloud-serverless/sca/releases/download/v0.0.1/sca-mac
+wget -O ~/.sca/sca https://github.com/jdcloud-serverless/sca/releases/download/v0.0.1/sca-mac
 
 chmod +x ~/.sca/sca
 
